ce01a810c404cae73b5df191043db0996b53ce0e
julio-silveira <julioqueiroz15@gmail.com>
2024-02-29 19:40:09 -0300

    chore: added biomejs to linting

--- api/src/routes/health.ts
+++ api/src/routes/health.ts
@@ -4,10 +4,10 @@ import statusCodes from "../utils/statusCode";
 const router = Router();
 
 router.get("/", (req, res) => {
-  const response = {
-    status: "ok",
-  };
-  return res.status(statusCodes.OK).json(response);
+	const response = {
+		status: "ok",
+	};
+	return res.status(statusCodes.OK).json(response);
 });
 
 export default router;
