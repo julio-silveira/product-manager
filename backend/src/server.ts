a48638b41fe7a7e7a026d86d2e8ef2226d3db92d
julio-silveira <julioqueiroz15@gmail.com>
2024-02-29 22:46:02 -0300

    chore: init vercel and renamed folder api > backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
 import app from "./app";
-import env from "./config/env";
 import db from "./database/db";
 
 const start = async () => {
 	try {
 		console.log("Connecting to database...");
 		await db.sync({ alter: true });
-		app.listen(3000);
-		console.log("Server is running on port 3000");
+		app.listen(3000, () => {
+			console.log(`Server is running on port ${3000}`);
+		});
 	} catch (error) {
 		console.error(error);
 		process.exit(1);
