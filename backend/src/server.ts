1d57911f92079efdf2380a2f5ab5c916db86d353
julio-silveira <julioqueiroz15@gmail.com>
2024-03-02 12:32:50 -0300

    feat: close instance on error

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,12 @@
 import app from "./app";
 import env from "./config/env";
-import { loadDatabase } from "./database/db";
 
 const start = async () => {
-	let sequelize = null;
 	try {
-		console.log("Connecting to database...");
-		sequelize = await loadDatabase();
 		app.listen(env.PORTS.API_PORT, () => {
 			console.log(`Server is running on port ${env.PORTS.API_PORT}`);
 		});
 	} catch (error) {
-		if (sequelize) {
-			await sequelize.close();
-		}
 		console.error(error);
 		process.exit(1);
 	}
