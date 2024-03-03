23bf7bb0375819ac93ef89a9ab8dd6565b0ef028
julio-silveira <julioqueiroz15@gmail.com>
2024-03-03 11:28:23 -0300

    feat: add currency formatter, revalidate products, and update theme toggle

--- frontend/src/services/http/products/productsApi.ts
+++ frontend/src/services/http/products/productsApi.ts
@@ -1,5 +1,6 @@
+import { AxiosError } from "axios";
 import { api } from "../../api";
-import { Product } from "./types";
+import { CreateProductResponse, CreateProductValues, Product } from "./types";
 
 const COMMON_PATH = "/products";
 
@@ -8,8 +9,33 @@ const getAll = async (): Promise<Product[]> => {
 	return data;
 };
 
+const create = async (
+	product: CreateProductValues,
+): Promise<CreateProductResponse> => {
+	try {
+		const { data } = await api.post(`${COMMON_PATH}`, product);
+		const message = data.message || "Product created successfully";
+		return { message, success: true };
+	} catch (e) {
+		const err = e as AxiosError<{ message: string }>;
+		const message = err?.response?.data?.message || "Failed to create product";
+
+		return { message, success: false };
+	}
+};
+
+const del = async (id: string): Promise<void> => {
+	const { status } = await api.delete(`${COMMON_PATH}/${id}`);
+
+	if (status !== 204) {
+		throw new Error("Failed to delete product, please try again later.");
+	}
+};
+
 const productsApi = {
 	getAll,
+	create,
+	del,
 };
 
 export default productsApi;
--- frontend/src/services/http/products/types.ts
+++ frontend/src/services/http/products/types.ts
@@ -1,3 +1,20 @@
+import { z } from "zod";
+
+export const CreateProductSchema = z.object({
+	name: z.string().min(3, { message: "Name must be at least 3 characters" }),
+	brand: z.string().min(3, { message: "Brand must be at least 3 characters" }),
+	model: z.string().min(3, { message: "Model must be at least 3 characters" }),
+	price: z.coerce.number().min(0, { message: "Price must be at least 0" }),
+	color: z.string().min(3, { message: "Color must be at least 3 characters" }),
+});
+
+export type CreateProductValues = z.infer<typeof CreateProductSchema>;
+
+export type CreateProductResponse = {
+	message: string;
+	success: boolean;
+};
+
 export interface Product {
 	id: number;
 	name: string;
